65915bd57d81c75f1958a26410ebc58c2d85aec7
crealu <cameron.miller294@gmail.com>
2023-12-21 21:43:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -6,19 +6,26 @@ const urls = {
 	planet: 'https://swapi.dev/api/planets/'
 }
 
-function renderData(name, value, level = 0) {
+function render(name, value, level = 0) {
 	let htmlString = `<div class="group level-${level}">${name} : `;
 	
 	if (typeof(value) == 'object' && value != undefined) {
 		htmlString += `<button class="expand"></button>`;
 		Object.keys(value).forEach(key => {
-			htmlString += renderData(key, value[key], level + 1) 
+			htmlString += render(key, value[key], level + 1) 
 		});
 	} else {
 		htmlString += value;
 	}
 
 	return htmlString + '</div>';
+}
+
+function assignListeners() {
+	const btns = document.getElementsByClassName('expand');
+	for (let i = 0; i < btns.length; i++) {
+		btns[i].addEventListener('click', handleClick);
+	}
 }
 
 function handleClick(event) {
@@ -30,33 +37,19 @@ function handleClick(event) {
 	})
 }
 
-function assignListeners() {
-	const btns = document.getElementsByClassName('expand');
-	for (let i = 0; i < btns.length; i++) {
-		btns[i].addEventListener('click', handleClick);
-	}
-}
-
 function fetchData(event) {
 	const content = event.target.textContent.toLowerCase();
-	let url = urls[content];
-
-	if (content == 'planet') {
-		const random = Math.floor(Math.random() * 20 + 1);
-		url = urls[content] + random;
-	}
+	const random = Math.floor(Math.random() * 20 + 1);
+	const url = content == 'planet' ? urls[content] + random : urls[content];
 
 	fetch(url)
 		.then(res => res.json())
 		.then(data => {
+			data = content == 'user' ? data.results[0] : data;
 			wrapper.innerHTML = '';
-			wrapper.innerHTML += content == 'user'
-				? renderData('user', data.results[0])
-				: renderData('planet', data);
+			wrapper.innerHTML += render(content, data);
 			assignListeners();
 		})
 }
 
-
-fetchButtons[0].addEventListener('click', fetchData);
-fetchButtons[1].addEventListener('click', fetchData);
+[...fetchButtons].forEach(btn => btn.addEventListener('click', fetchData));
